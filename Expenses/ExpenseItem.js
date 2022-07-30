c91c8d6512830dcd43805de67a7c4be9396b98ab
Ankit Roy <90422666+royankit15@users.noreply.github.com>
2022-07-30 10:42:18 +0530

    Task-4

--- Expenses/ExpenseItem.js
+++ Expenses/ExpenseItem.js
@@ -1,17 +1,28 @@
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css'
+import React, {useState} from 'react';
 
-function ExpenseItem(props)
-{ 
-   
-    return (
-        <div className='expense-item'>
-        <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-           <h2>{props.title}</h2> 
-       <div className ='expense-item__price'>${props.amount}</div>
-        </div>
-        </div>
-    );
-}   
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+
+const ExpenseItem = (props) => {
+  
+  const [title, setTitle] =useState(props.title);
+
+  const ClickHandler =() => {
+    setTitle ('Updated')
+    console.log(title);   
+};
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick ={ClickHandler}>Change Title</button>
+    </Card>
+  );
+}
+
 export default ExpenseItem;
